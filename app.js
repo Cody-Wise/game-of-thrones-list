4f814c648f80fd6ce5a5cf0310fb74807b1c5554
Cody-Wise <cody@sharptackmedia.com>
2022-03-22 16:08:23 -0700

    Completed fetch and render utils

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 // import functions and grab DOM elements
 
+import { getHeroes } from './fetch-utils.js';
+import { renderHeroes } from './render-utils.js';
+
+const heroListEl = document.querySelector('got-list');
+
+window.addEventListener('load', async () => {
+
+    const heroes = await getHeroes();
+
+    for (let hero of heroes){
+
+      const heroEl = renderHeroes(hero);
+    }
+  
+});
+
+
 // let state
+
+
 
 // set event listeners 
   // get user input
