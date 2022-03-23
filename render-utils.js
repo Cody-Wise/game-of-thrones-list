4249dcc419988dd5127c1a1f4b78d8ed39546791
Cody-Wise <cody@sharptackmedia.com>
2022-03-22 17:48:36 -0700

    Fixed css Bugs

--- render-utils.js
+++ render-utils.js
@@ -1,7 +1,7 @@
 export function renderHeroes(hero){
     const heroEl = document.createElement('div');
     const a = document.createElement('a');
-    const nameEl = document.createElement('h4');
+    const nameEl = document.createElement('h3');
     const imageEl = document.createElement('img');
     // const birthYearEl = document.createElement('p');
     // const genderEl = document.createElement('p');
@@ -11,13 +11,13 @@ export function renderHeroes(hero){
 
     heroEl.classList.add('hero');
 
-    nameEl.innerHTML = `<b>Character Name:</b> ${hero.character_name}`;
+    nameEl.innerHTML = `<b>${hero.character_name}</b>`;
     // birthYearEl.innerHTML = `<b>Birth Year:</b> ${hero.birthyear}`;
     // genderEl.innerHTML = `<b>Gender:</b> ${hero.gender}`;
     // houseNameEl.innerHTML = `<b>House Name: ${hero.house_name}`;
     // descriptionEl.innerHTML = `<b>Character Info:</b> ${hero.description}`;
     imageEl.src = `${hero.image}`;
-    a.href = `.detail/?id=${hero.person_id}`;
+    a.href = `./detail/?id=${hero.person_id}`;
 
     heroEl.append(nameEl, imageEl);
 
@@ -31,7 +31,7 @@ export function renderHeroes(hero){
 export function renderHeroesDetail(hero){
 
     const heroEl = document.createElement('div');
-    const nameEl = document.createElement('h4');
+    const nameEl = document.createElement('h2');
     const imageEl = document.createElement('img');
     const birthYearEl = document.createElement('p');
     const genderEl = document.createElement('p');
@@ -43,12 +43,12 @@ export function renderHeroesDetail(hero){
 
     heroEl.classList.add('hero-detail');
 
-    nameEl.innerHTML = `<b>Character Name:</b> ${hero.name}`;
+    nameEl.innerHTML = `<b>${hero.character_name}</b>`;
     birthYearEl.innerHTML = `<b>Birth Year:</b> ${hero.birthyear}`;
     genderEl.innerHTML = `<b>Gender:</b> ${hero.gender}`;
-    houseNameEl.innerHTML = `<b>House Name: ${hero.house_name}`;
+    houseNameEl.innerHTML = `<b>House Name:</b> ${hero.house_name}`;
     descriptionEl.innerHTML = `<b>Character Info:</b> ${hero.description}`;
-    imageEl.src = 'hero.image';
+    imageEl.src = `${hero.image}`;
 
     heroEl.append(nameEl, imageEl, birthYearEl, genderEl, houseNameEl, descriptionEl);
 
